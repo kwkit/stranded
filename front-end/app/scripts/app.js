b1388cca2665c8cf9fa1a76d872da9460b0d16cc
Lei Mingyu <uygnim.iel@gmail.com>
2015-09-14 02:16:26 +0800

    add login button, clear unused file

--- front-end/app/scripts/app.js
+++ front-end/app/scripts/app.js
@@ -44,44 +44,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
     }
   })
 
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
-
-  .state('app.browse', {
-    url: '/browse',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/browse.html'
-      }
-    }
-  })
-
-  .state('app.playlists', {
-    url: '/playlists',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlists.html',
-        controller: 'PlaylistsCtrl'
-      }
-    }
-  })
-
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  })
-
   .state('app.login', {
     url: '/login',
     views: {
--- front-end/app/scripts/controllers.js
+++ front-end/app/scripts/controllers.js
@@ -31,19 +31,4 @@ angular.module('starter.controllers', [])
     var user = usersApi.createUser($scope.signUpData);
     console.log(user);
   };
-})
-
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
-
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-  console.log($stateParams);
 });
