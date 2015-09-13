106daa463e17f0232dcc33040359f60e7b5be74a
Koh Wai Kit <kwkit1992@gmail.com>
2015-09-13 13:55:46 +0800

    Add fish action

--- api/app/controllers/api/v1/bottles_controller.rb
+++ api/app/controllers/api/v1/bottles_controller.rb
@@ -1,6 +1,12 @@
 class Api::V1::BottlesController < ApplicationController
-  before_action :authenticate_with_token!, only: [:create]
+  before_action :authenticate_with_token!, only: [:create, :fish]
   respond_to :json
+
+  def fish
+    bottle = Bottle.where(opened: false).order("RANDOM()").first
+    bottle.update(opened: true)
+    respond_with bottle
+  end
 
   def create
     bottle = current_user.bottles.build(bottle_params)
