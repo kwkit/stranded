bb2053520c777044f4d82bcec2a09a0a4e8ec6a2
Sue Zheng Hao <szhlibrary@gmail.com>
2015-09-13 17:13:52 +0800

    Angular Sessions API: WIP ><

---
+++ front-end/app/scripts/factories/sessionsApi.js
@@ -0,0 +1,23 @@
+'use strict';
+angular.module('starter.services', [])
+  .factory('sessionsApi', function($resource){
+    var sessionsApi = $resource('', {}, {
+      'createSession': {
+        method: 'POST',
+        url: 'api.localhost/sessions',
+        isArray: true
+      },
+      'destroySession': {
+        method: 'DELETE',
+        url: 'api.localhost/sessions/:id'
+      }
+    });
+    return {
+      createSession: function(data) {
+        return sessionsApi.createSession(data);
+      },
+      destroySession: function(data){
+        return sessionsApi.destroySession(data);
+      }
+    };
+  });
