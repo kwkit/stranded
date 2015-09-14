d581daca9981fb8c86f7bf642f9f475dc5db3978
Koh Wai Kit <kwkit1992@gmail.com>
2015-09-14 17:49:05 +0800

    Fix middleware

--- api/config/application.rb
+++ api/config/application.rb
@@ -19,7 +19,7 @@ module Stranded
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.middleware.insert_before ActionDispatch::Static, "Rack::Cors" do
+    config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins 'localhost:8100'
         resource '*',
