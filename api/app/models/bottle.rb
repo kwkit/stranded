da5f5e4d58f68516dceaf39162f472037a7d0238
Koh Wai Kit <kwkit1992@gmail.com>
2015-09-19 01:09:29 +0800

    Add author field to bottles

--- api/app/models/bottle.rb
+++ api/app/models/bottle.rb
@@ -1,5 +1,6 @@
 class Bottle < ActiveRecord::Base
   validates :message, presence: true
+  validates :author, presence: true
   belongs_to :user
   has_many :messages
   validates_associated :messages
