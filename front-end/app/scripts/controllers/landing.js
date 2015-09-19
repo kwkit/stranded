7625848f7be99a7db9a92aef4d40f9982af85a5d
Lei Mingyu <uygnim.iel@gmail.com>
2015-09-20 03:10:54 +0800

    Landing: show different text

--- front-end/app/scripts/controllers/landing.js
+++ front-end/app/scripts/controllers/landing.js
@@ -1,18 +1,23 @@
 'use strict';
 
 angular.module('stranded.controllers')
-  .controller('LandingCtrl', function ($scope, $interval) {
+  .controller('LandingCtrl', function ($scope, $interval, $timeout) {
     $scope.slogans = [
       'What if you were stranded on an island?',
-      'What if your only means of communication was sending messages in a bottle to strangers?',
+      'What if you can only send messages in a bottle to strangers?',
       'Log in and experience a whole new type of anonymous messaging!'
     ];
 
     let i = 0;
+    $scope.showSlogan = true;
     $scope.slogan = $scope.slogans[0];
     $interval(function () {
       i = (++ i) % 3;
-      $scope.slogan = $scope.slogans[i];
+      $scope.showSlogan = false;
+      $timeout(function () {
+        $scope.slogan = $scope.slogans[i];
+        $scope.showSlogan = true;
+      }, 500)
     }, 3000);
 
   });
