d7c62e3e24501fa3f43244c838ce91d9e45bdd98
Lei Mingyu <uygnim.iel@gmail.com>
2015-09-20 02:40:06 +0800

    slogan added

--- front-end/app/scripts/controllers/landing.js
+++ front-end/app/scripts/controllers/landing.js
@@ -1,6 +1,18 @@
 'use strict';
 
 angular.module('stranded.controllers')
-  .controller('LandingCtrl', function ($scope) {
-    console.log($scope);
+  .controller('LandingCtrl', function ($scope, $interval) {
+    $scope.slogans = [
+      'What if you were stranded on an island?',
+      'What if your only means of communication was sending messages in a bottle to strangers?',
+      'Log in and experience a whole new type of anonymous messaging!'
+    ];
+
+    let i = 0;
+    $scope.slogan = $scope.slogans[0];
+    $interval(function () {
+      i = (++ i) % 3;
+      $scope.slogan = $scope.slogans[i];
+    }, 3000);
+
   });
