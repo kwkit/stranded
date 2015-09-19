97413636d194be2e93d2db228a3f911dcc3b31d7
Sue Zheng Hao <szhlibrary@gmail.com>
2015-09-19 18:10:59 +0800

    Signup: Shows an (unstyled) alert upon successful signup.

--- front-end/app/scripts/controllers/signup.js
+++ front-end/app/scripts/controllers/signup.js
@@ -1,13 +1,27 @@
 'use strict';
 
 angular.module('stranded.controllers')
-  .controller('SignUpCtrl', function ($scope, usersApi) {
+  .controller('SignUpCtrl', function ($scope, $state, $ionicPopup, usersApi) {
     $scope.signUpData = {};
     $scope.doSignUp = function () {
       console.log('Doing signup', $scope.signUpData);
 
       // TODO: Authentication code here
-      var user = usersApi.createUser($scope.signUpData);
+      var user = usersApi.createUser($scope.signUpData).$promise.then(
+        function (response) {
+          console.log(response);
+          var alertPopup = $ionicPopup.alert({
+            title: 'Successfully signed up!',
+            template: 'Please proceed to log in.'
+          });
+          alertPopup.then(function() {
+            $state.go('login');
+          });
+        },
+        function (error) {
+          console.log('Error:', error);
+        }
+      );
       console.log(user);
     };
   });
