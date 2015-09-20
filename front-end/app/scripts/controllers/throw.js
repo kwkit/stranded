304d009a581cc9eabecf201e81e49e91fd807efa
Sue Zheng Hao <szhlibrary@gmail.com>
2015-09-20 17:11:46 +0800

    New Bottle: WIP. Unstyled page. TODO: Ask for geolocation permissions using the toggle (which presently does nothing).

---
+++ front-end/app/scripts/controllers/throw.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('stranded.controllers')
+  .controller('ThrowCtrl', function ($scope, bottlesApi) {
+    $scope.newMessageData = {};
+    $scope.doCreateBottle = function () {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          function (position) {
+            $scope.newMessageData.latitude = position.coords.latitude;
+            $scope.newMessageData.longitude = position.coords.longitude;
+          }
+        );
+      } else {
+        console.log("Geolocation is not supported by this browser.");
+      }
+
+      bottlesApi.createBottle($scope.newMessageData).$promise.then(
+        function (response) {
+          console.log(response);
+        },
+        function (error) {
+          console.log('Error:', error.errors);
+        }
+      );
+    };
+  });
