65e18aae745ae97baa65278d790649201d0d6630
Sue Zheng Hao <szhlibrary@gmail.com>
2015-09-20 14:25:51 +0800

    Grab Bottle: Primitive implementation (only logs response to console)

--- front-end/app/scripts/controllers/grab.js
+++ front-end/app/scripts/controllers/grab.js
@@ -1,6 +1,17 @@
 'use strict';
 
 angular.module('stranded.controllers')
-  .controller('GrabCtrl', function () {
-    console.log('grabbbb');
+  .controller('GrabCtrl', function ($scope, bottlesApi) {
+
+    $scope.grabBottle = function () {
+      console.log('grabbing bottle');
+      var bottle = bottlesApi.fishBottle().$promise.then(
+        function (response) {
+          console.log(response);
+        },
+        function (error) {
+          console.log('Error:', error.errors);
+        }
+      );
+    };
   });
