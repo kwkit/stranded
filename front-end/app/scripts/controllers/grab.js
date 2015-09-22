a209daee6f650473e1fd27dbf159d073a10c829e
Sue Zheng Hao <szhlibrary@gmail.com>
2015-09-22 14:56:16 +0800

    Grab: Show loading indicator only right before a promise.

--- front-end/app/scripts/controllers/grab.js
+++ front-end/app/scripts/controllers/grab.js
@@ -23,8 +23,10 @@ angular.module('stranded.controllers')
       if ($scope.currentBottle) {
         console.log('you already have a bottle, this function shouldn\'t be called');
       } else {
+        $ionicLoading.show();
         bottlesApi.fishBottle().$promise.then(
           function (response) {
+            $ionicLoading.hide();
             $scope.currentBottle = response.bottle;
             console.log(response);
           },
@@ -66,11 +68,10 @@ angular.module('stranded.controllers')
     $scope.releaseBottle = function () {
       console.log('releasing bottle');
 
-      $ionicLoading.show();
-
       if (!$scope.currentBottle) {
         console.log('you don\'t have a bottle to release, this function shouldn\'t be called');
       } else {
+        $ionicLoading.show();
         bottlesApi.releaseBottle().$promise.then(
           function (response) {
             console.log(response);
