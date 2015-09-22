2220b74365788f105b339d08bf3300af6869b296
Lei Mingyu <uygnim.iel@gmail.com>
2015-09-22 22:54:44 +0800

    calculate the travelled distance

--- front-end/app/scripts/controllers/demomap.js
+++ front-end/app/scripts/controllers/demomap.js
@@ -62,4 +62,30 @@ angular.module('stranded.controllers')
       }
     });
 
+    function distanceBetween(lng1, lat1, lng2, lat2) {
+      function toRad(number) {
+        return number * Math.PI / 180;
+      }
+
+      var radius = 6371000; // metres
+      var phi1 = toRad(lat1);
+      var phi2 = toRad(lat2);
+      var deltaPhi = toRad((lat2 - lat1));
+      var deltaLamda = toRad((lng2 - lng1));
+
+      var a = Math.sin(deltaPhi / 2) * Math.sin(deltaPhi / 2) +
+              Math.cos(phi1) * Math.cos(phi2) *
+              Math.sin(deltaLamda / 2) * Math.sin(deltaLamda / 2);
+      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+      return radius * c;
+    }
+
+    $scope.distance = 0;
+    for (var i = 1; i < $scope.paths.length; i++) {
+      var location1 = $scope.paths[i];
+      var location2 = $scope.paths[i - 1];
+      $scope.distance += distanceBetween(location1[1], location1[0], location2[1], location2[0]);
+    }
+
   });
