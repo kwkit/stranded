4fcdc92a6a5c1c5e71f4f49f2c7bd2ca5e1b61eb
Koh Wai Kit <kwkit1992@gmail.com>
2015-09-23 23:22:13 +0800

    Enable bottle stars on front end

--- api/app/serializers/bottle_serializer.rb
+++ api/app/serializers/bottle_serializer.rb
@@ -1,6 +1,6 @@
 class BottleSerializer < ActiveModel::Serializer
   attr_accessor :current_user_id
-  attributes :message, :author, :latitude, :longitude, :created_at, :messages
+  attributes :id, :message, :author, :latitude, :longitude, :created_at, :messages
   def attributes
     data = super
     data[:stars] = object.bottle_stars.count
