460632d32edb2c5940fcd5659e8a08af33049a9d
Sue Zheng Hao <szhlibrary@gmail.com>
2015-09-25 23:17:39 +0800

    Track: Show message if offline.

--- front-end/app/scripts/controllers/track.js
+++ front-end/app/scripts/controllers/track.js
@@ -1,16 +1,27 @@
 'use strict';
 
 angular.module('stranded.controllers')
-  .controller('TrackCtrl', function ($scope, $state, $ionicLoading, $ionicPopup, bottlesApi) {
+  .controller('TrackCtrl', function ($scope, $rootScope, $state, $ionicLoading, $ionicPopup,
+                                     localStorageService, bottlesApi) {
     $scope.getMyBottles = function() {
-      bottlesApi.getMyBottles().$promise.then(
-        function (response) {
-          $scope.myBottles = response.bottles;
-        },
-        function (error) {
-          console.log('Error:', error);
+      if ($rootScope.online) {
+        bottlesApi.getMyBottles().$promise.then(
+          function (response) {
+            $scope.myBottles = response.bottles;
+            localStorageService.set('myBottles', angular.toJson($scope.myBottles));
+          },
+          function (error) {
+            console.log('Error:', error);
+          }
+        );
+      } else {
+        if (localStorageService.get('myBottles')) {
+          $scope.myBottles = angular.fromJson(localStorageService.get('myBottles'));
+        } else {
+
         }
-      );
+      }
+
     };
     $scope.$on('$ionicView.enter', function() {
       $scope.getMyBottles(); 
